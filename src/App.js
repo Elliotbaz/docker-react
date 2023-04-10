0865d9e012ec4f90c5db25981712ec5cd934c059
Elliot Bazuaye <obazuaye55@gmail.com>
2023-04-10 04:19:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nigga moment part 1 and 2
         </p>
         <a
           className="App-link"
