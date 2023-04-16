a62c4d2c4e242af34a8d387ede6f17f41de7e0da
Elliot Bazuaye <obazuaye55@gmail.com>
2023-04-16 16:15:35 -0600

    okay

--- src/index.js
+++ src/index.js
@@ -12,6 +12,6 @@ root.render(
 );
 
 // If you want to start measuring performance in your app
-//okay it is me again last time argj
+//okay it is me again last time argj h
 // to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
