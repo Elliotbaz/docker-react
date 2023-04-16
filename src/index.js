d3961c91df7f90b75bc4646ac4db4e805883710d
Elliot Bazuaye <obazuaye55@gmail.com>
2023-04-16 07:58:03 -0600

    just adding

--- src/index.js
+++ src/index.js
@@ -12,5 +12,6 @@ root.render(
 );
 
 // If you want to start measuring performance in your app
+//okay it is me again
 // to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
